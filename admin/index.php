93b18695dd371ccff87a62a836c5e5c9c115c967
Ebrahemsamer2 <ebrahemsamer2@gmail.com>
2019-01-26 09:59:47 +0200

    Design Posts Table

--- admin/index.php
+++ admin/index.php
@@ -13,7 +13,56 @@ $dashboard = "active";
         </div>
         <div class="col-sm">
             <div class='main-area'>
-                 
+
+                <div class='row'>
+                    <div class="col-sm-6">
+                        <div class="table-responsive">
+                            <table class="table table-striped table-hover table-dark">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td colspan="2">Larry the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        
+                    </div>
+                </div>
+
+                <div class='row'>
+                    <div class="col-sm-6">
+                        
+                    </div>
+                    <div class="col-sm-6">
+                        
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </div>
